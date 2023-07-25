495574ecae2ec217e7f8d9f278dbe2d06394f7eb
Phil-Pfister <salshouse@gmail.com>
2023-07-24 21:45:02 -0500

    cleaned up code

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-// import './App.css';
-// import { Outlet } from 'react-router-dom';
+
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
